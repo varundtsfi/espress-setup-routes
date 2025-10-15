2027bf928e398e23695ed493745ff5f68e23623c
Varun Kumar <varun.kumar@crestechglobal.com>
2025-10-15 17:39:57 +0530

    add the validate_task_id.sh script

---
+++ scripts/validate_task_id.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Bash sets the BASH environment variable, so if it is not set, then we
+# are running in a different shell, so manually run ourselves in BASH.
+if [ -z "${BASH:-}" ]; then
+	exec bash "$0" "$@"
+fi
+
+. "$(dirname "$0")/helpers.sh"
+
+# validate task id in commit message
+
+current_commit_msg_file="$1"
+validate_commit_msg_task_id "$current_commit_msg_file" || exit 1
